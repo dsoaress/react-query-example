4ba510d525fa1984b337c7fc3efaecff9bde6d18
Daniel Soares <daniel.soares@me.com>
2022-01-28 11:26:43 -0300

    feat: add link to header logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import { Box, Flex, HStack } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 
 import { Logo } from '../assets/Logo'
 import { NavLink } from './NavLink'
 import { ProfileMenu } from './ProfileMenu'
 
 export function Header() {
+  const navigate = useNavigate()
+
   return (
     <Box
       as="header"
@@ -17,7 +20,7 @@ export function Header() {
       zIndex="banner"
     >
       <Flex justify="space-between" align="center" maxW="container.xl" mx="auto">
-        <Logo color="white" h="6" />
+        <Logo color="white" h="6" cursor="pointer" onClick={() => navigate('/dashboard')} />
         <HStack as="nav" spacing={4}>
           <NavLink to="students">Students</NavLink>
           <NavLink to="professors">Professors</NavLink>
